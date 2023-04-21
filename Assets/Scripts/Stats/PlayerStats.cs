2555113182701a7082c07f22dc128bdf1e3fc7cc
Tiberius2 <tiberiutiprigan112@gmail.com>
2023-04-21 19:18:35 +0300

    Reorganized some scripts

--- Assets/Scripts/Stats/PlayerStats.cs
+++ Assets/Scripts/Stats/PlayerStats.cs
@@ -18,8 +18,8 @@ public class PlayerStats : CharacterStats
 
         if(oldItem != null)
         {
-            armor.RemoveModifier(newItem.armorModifier);
-            damage.RemoveModifier(newItem.damageModifier);
+            armor.RemoveModifier(oldItem.armorModifier);
+            damage.RemoveModifier(oldItem.damageModifier);
         }
     }
 
